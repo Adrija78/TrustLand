902119279951970ef8ff9e282c64a3762206846c
Adrija78 <adrijak84@gmail.com>
2026-07-02 16:30:52 +0530

    Improve production

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,4 +1,7 @@
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config = {
   solidity: {
@@ -14,7 +17,16 @@ const config = {
     hardhat: {},
     localhost: {
       url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY && process.env.PRIVATE_KEY.startsWith("0x") || (process.env.PRIVATE_KEY && process.env.PRIVATE_KEY.length === 64) 
+        ? [process.env.PRIVATE_KEY] 
+        : [],
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
 
